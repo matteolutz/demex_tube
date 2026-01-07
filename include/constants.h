b953f970035f5fea80f375a08956b62f713acd80
matteolutz <matteo.lutz06@outlook.de>
2026-01-07 17:01:54 +0100

    chore: switch to 25 pixel rgbw

--- include/constants.h
+++ include/constants.h
@@ -6,11 +6,11 @@
 
 #define FORCE_AP_MODE_PIN D5
 
-// #define NUM_LEDS 25
-#define NUM_LEDS 20
+#define NUM_LEDS 25
+// #define NUM_LEDS 20
 #define LED_PIN D2
-// #define LED_TYPE (NEO_WRGB + NEO_KHZ800)
-#define LED_TYPE (NEO_GRB + NEO_KHZ800)
+#define LED_TYPE (NEO_WRGB + NEO_KHZ800)
+// #define LED_TYPE (NEO_GRB + NEO_KHZ800)
 
 #define MIN_LED_UPDATE_INTERVAL_MS 10
 
